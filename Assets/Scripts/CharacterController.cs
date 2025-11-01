447d9252f1c926a1112a470647bad273b67a458e
Hik <luizhenrique98@gmail.com>
2025-11-01 15:32:23 -0300

    chore: remove namespaces, clean file

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,59 +1,54 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace DefaultNamespace
+public class CharacterController : MonoBehaviour
 {
-    public class CharacterController : MonoBehaviour
+    [SerializeField] private CharacterMotor _motor;
+    [SerializeField] private float _bufferTime = 0.2f;
+
+    private float _inputLastUpdatedAt = float.MaxValue;
+    private Vector2Int? _inputBuffer;
+    private Vector2Int? _currentDirection;
+
+    private void Start()
     {
-        [SerializeField] private CharacterMotor _motor;
-        [SerializeField] private float _bufferTime = 0.2f;
+        InputSystem.actions.FindAction("Move").performed += StoreInput;
+        InputSystem.actions.FindAction("Move").canceled += _ => _inputLastUpdatedAt = Time.time;
+    }
 
-        private float _inputLastUpdatedAt = float.MaxValue;
-        private Vector2Int? _inputBuffer;
-        private Vector2Int? _currentDirection;
-
-        private void Start()
+    private void Update()
+    {
+        if (Time.time > _inputLastUpdatedAt + _bufferTime)
         {
-            InputSystem.actions.FindAction("Move").performed += StoreInput;
-            InputSystem.actions.FindAction("Move").canceled += _ => _inputLastUpdatedAt = Time.time;
+            _inputBuffer = null;
         }
 
-        private void Update()
+        if (_inputBuffer.HasValue && !_motor.IsBusy)
         {
-            if (Time.time > _inputLastUpdatedAt + _bufferTime)
+            if (_motor.TryMoveTowards(_inputBuffer.Value))
             {
-                _inputBuffer = null;
+                _currentDirection = _inputBuffer.Value;
             }
-
-            if (_inputBuffer.HasValue && !_motor.IsBusy)
+            else if (_currentDirection.HasValue)
             {
-                if (_motor.TryMoveTowards(_inputBuffer.Value))
-                {
-                    _currentDirection = _inputBuffer.Value;
-                }
-                else if (_currentDirection.HasValue)
-                {
-                    _motor.TryMoveTowards(_currentDirection.Value);
-                }
+                _motor.TryMoveTowards(_currentDirection.Value);
             }
         }
+    }
 
-        private void StoreInput(InputAction.CallbackContext ctx)
+    private void StoreInput(InputAction.CallbackContext ctx)
+    {
+        var rawInput = Vector2Int.RoundToInt(ctx.ReadValue<Vector2>());
+        if (Mathf.Abs(rawInput.x) > Mathf.Abs(rawInput.y))
         {
-            var rawInput = Vector2Int.RoundToInt(ctx.ReadValue<Vector2>());
-            if (Mathf.Abs(rawInput.x) > Mathf.Abs(rawInput.y))
-            {
-                rawInput = new Vector2Int(rawInput.x, 0);
-            }
-            else
-            {
-                rawInput = new Vector2Int(0, rawInput.y); 
-            }
-
-            _inputBuffer = rawInput;
-            _inputLastUpdatedAt = float.MaxValue;
+            rawInput = new Vector2Int(rawInput.x, 0);
         }
+        else
+        {
+            rawInput = new Vector2Int(0, rawInput.y); 
+        }
+
+        _inputBuffer = rawInput;
+        _inputLastUpdatedAt = float.MaxValue;
     }
 }
--- Assets/Scripts/CharacterMotor.cs
+++ Assets/Scripts/CharacterMotor.cs
@@ -1,52 +1,49 @@
 using System;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class CharacterMotor : MonoBehaviour
 {
-    public class CharacterMotor : MonoBehaviour
+    [SerializeField] private GameMap _map;
+    [SerializeField] private float _speed;
+
+    private Vector2Int? _targetPosition;
+    
+    public bool IsBusy =>  _targetPosition.HasValue;
+
+    public bool TryMoveTowards(Vector2Int direction)
     {
-        [SerializeField] private GameMap _map;
-        [SerializeField] private float _speed;
-
-        private Vector2Int? _targetPosition;
-        
-        public bool IsBusy =>  _targetPosition.HasValue;
-
-        public bool TryMoveTowards(Vector2Int direction)
+        if (IsBusy)
         {
-            if (IsBusy)
-            {
-                Debug.LogError("Trying to move character in mid movement");
-                return false;
-            }
-
-            if (direction == Vector2Int.zero)
-            {
-                throw new ArgumentNullException("Trying to move with zero direction");
-            }
-
-            Vector2Int desiredPosition =
-                Vector2Int.FloorToInt(new Vector2(transform.position.x, transform.position.z)) +
-                direction;
-            
-            if (!_map.IsTileEmpty(desiredPosition)) return false;
-
-            _targetPosition = desiredPosition;
-            return true;
+            Debug.LogError("Trying to move character in mid movement");
+            return false;
         }
 
-        private void Update()
+        if (direction == Vector2Int.zero)
         {
-            if (_targetPosition == null) return;
+            throw new ArgumentNullException("Trying to move with zero direction");
+        }
 
-            Vector3 delta = new Vector3(_targetPosition.Value.x + 0.5f, transform.position.y, _targetPosition.Value.y + 0.5f) - transform.position;
-            Vector3 amountToMove = Vector3.ClampMagnitude(Time.deltaTime * _speed * delta.normalized, delta.magnitude);
-            
-            transform.position += amountToMove;
-            if (amountToMove == delta)
-            {
-                _targetPosition = null;
-            }
+        Vector2Int desiredPosition =
+            Vector2Int.FloorToInt(new Vector2(transform.position.x, transform.position.z)) +
+            direction;
+        
+        if (!_map.IsTileEmpty(desiredPosition)) return false;
+
+        _targetPosition = desiredPosition;
+        return true;
+    }
+
+    private void Update()
+    {
+        if (_targetPosition == null) return;
+
+        Vector3 delta = new Vector3(_targetPosition.Value.x + 0.5f, transform.position.y, _targetPosition.Value.y + 0.5f) - transform.position;
+        Vector3 amountToMove = Vector3.ClampMagnitude(Time.deltaTime * _speed * delta.normalized, delta.magnitude);
+        
+        transform.position += amountToMove;
+        if (amountToMove == delta)
+        {
+            _targetPosition = null;
         }
     }
 }
