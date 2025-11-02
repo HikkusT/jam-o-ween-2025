f4b2641efe32db54c4118275661e5e941e24f4d1
Hik <luizhenrique98@gmail.com>
2025-11-01 22:17:28 -0300

    feat: add color variation to ghosts

--- Assets/Scripts/GhostVisuals.cs
+++ Assets/Scripts/GhostVisuals.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GhostVisuals : MonoBehaviour
 {
     [SerializeField] private Transform _view;
+    [SerializeField] private Renderer _mainRenderer;
     [SerializeField] private float _amplitude;
     [SerializeField] private float _frequency;
+    [SerializeField, ColorUsage(false, true)] private List<Color> _availableColors;
 
     private float _startingY;
 
     private void Start()
     {
         _startingY = _view.localPosition.y;
+        Color color = _availableColors[Random.Range(0, _availableColors.Count)];
+        _mainRenderer.material.SetColor("_BaseColor", color);
+        _mainRenderer.material.SetColor("_1st_ShadeColor", color * 0.8f);
+        _mainRenderer.material.SetColor("_Emissive_Color", new Color(color.r, color.g, color.b, 1.5f));
     }
 
     private void Update()
