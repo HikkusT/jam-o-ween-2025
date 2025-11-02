d5ca6e9a36e35583bf14822cc49aa8c0398cf14e
Lucasdho <lucasdfso@gmail.com>
2025-11-01 22:24:12 -0300

    player can now use guardian angel! and die too

--- Assets/Scripts/PlayerComponents/Health.cs
+++ Assets/Scripts/PlayerComponents/Health.cs
@@ -7,23 +7,18 @@ namespace PlayerComponents
     {
         [SerializeField] private int maxHealth = 5;
         private int _currentHealth;
+        private bool _isDead = false;
+        
         private PlayerCollisionHandler _playerCollisionHandler;
         
-        public event Action OnDeath;
+        public event EventHandler<DeathEventArgs> OnDeath;
+        public event Action OnHardDeath;
 
         private void Start()
         {
+            _currentHealth = maxHealth;
             _playerCollisionHandler  = GetComponent<PlayerCollisionHandler>();
             _playerCollisionHandler.OnTakeDamage += TakeDamage;
-        }
-
-        private void Update()
-        {
-            if (_currentHealth <= 0)
-            {
-                OnDeath?.Invoke();
-            }
-
         }
         
         public int GetPlayerHealth()
@@ -33,12 +28,13 @@ namespace PlayerComponents
         
         public void ChangeHealth(int amount)
         {
-            _currentHealth += amount;
+            _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, maxHealth);
         }
 
         public void ChangePermanentHealth(int amount)
         {
-            maxHealth += amount;
+            maxHealth = Mathf.Clamp(maxHealth + amount, 1, int.MaxValue);
+            ChangeHealth(amount);
         }
 
         public void RestoreFullLife()
@@ -48,7 +44,42 @@ namespace PlayerComponents
 
         public void TakeDamage()
         {
+            if (_isDead) return;
+            
             ChangeHealth(-1);
+            
+            int playerHealth = GetPlayerHealth();
+            if (playerHealth > 0) {
+                Debug.Log("TAKE DAMAGE");
+                Debug.Log("Health: " + playerHealth);
+            }
+            else
+            { 
+                AttemptToDie();
+            }
+        }
+
+        private void AttemptToDie()
+        {
+            DeathEventArgs args = new DeathEventArgs();
+
+            OnDeath?.Invoke(this, args);
+
+            if (args.IsSaved)
+            {
+                // A relic subscriber set 'IsSaved' to true.
+                Debug.Log("Player was SAVED from death by a relic!");
+            }
+            else
+            {
+                // No one saved the player. Proceed with death.
+                _isDead = true;
+                OnHardDeath?.Invoke();
+                Debug.Log("DEAD");
+                
+                // Put your REAL death logic here (e.g., play animation, show UI)
+                gameObject.SetActive(false); // Example: just disable the player
+            }
         }
         
     }
